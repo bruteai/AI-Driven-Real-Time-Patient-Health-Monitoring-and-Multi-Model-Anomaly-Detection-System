2d3ee301222b129d66e5f912a63fc590053cc029
bruteai <57473568+bruteai@users.noreply.github.com>
2025-04-08 03:57:31 -0500

    Delete src/deep_learning_model.py

--- src/deep_learning_model.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-
-class AnomalyDetector(nn.Module):
-    def __init__(self):
-        super(AnomalyDetector, self).__init__()
-        self.layer = nn.Linear(5, 1)
-    def forward(self, x):
-        return self.layer(x)
